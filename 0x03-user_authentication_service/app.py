660dd9c93e22fd0ca7430a1014c78433a5296790
x9x96 <israelsbook@gmail.com>
2024-01-22 12:48:21 -0800

    app task 3

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,6 +59,14 @@ def login() -> str:
     return resp
 
 
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """
+    Log out a logged in user and destroy their session
+    """
+    session_id = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None or session_id is None:
 
 
 if __name__ == "__main__":
