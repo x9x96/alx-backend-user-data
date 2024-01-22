126fceaa5fba516261de8800394f3d66eb12390d
x9x96 <israelsbook@gmail.com>
2024-01-22 12:52:51 -0800

    auth task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,3 +58,25 @@ class Auth:
             return usr
         raise ValueError(f"User {email} already exists")
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validate a user's login credentials and return True if they are correct
+        or False if they are not
+        Args:
+            email (str): user's email address
+            password (str): user's password
+        Return:
+            True if credentials are correct, else False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        user_password = user.hashed_password
+        passwd = password.encode("utf-8")
+        return bcrypt.checkpw(passwd, user_password)
+
+
+
+
