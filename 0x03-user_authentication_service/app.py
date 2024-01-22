f53bb7ded25e536d812208234af8f6adec0e5e4f
x9x96 <israelsbook@gmail.com>
2024-01-22 12:47:17 -0800

    app task 1

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -17,6 +17,30 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """
+    Return json respomse
+    {"message": "Bienvenue"}
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """
+    Register new users
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{email}", "message": "user created"})
+
+
 
 
 if __name__ == "__main__":
